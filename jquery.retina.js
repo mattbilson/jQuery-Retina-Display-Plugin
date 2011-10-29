c4e45c1a4862e80380f1fe63b872e9811d944581
Matt Bilson <matt@mattbilson.com>
2011-10-29 18:42:34 +1300

    Added check for if source has already been updated

--- jquery.retina.js
+++ jquery.retina.js
@@ -8,6 +8,7 @@
 		1.0 (23/08/2010)	- Initial release.
 		1.1 (27/08/2010)	- Made plugin chainable
 		1.2 (10/11/2010)	- Fixed broken retina_part setting. Wrapped in self executing function (closure)
+		1.3 (29/10/2011)	- Checked if source has already been updated
 */
 
 (function( $ ){
@@ -20,6 +21,9 @@
 		if(window.devicePixelRatio >= 2) {
 			this.each(function(index, element) {
 				if(!$(element).attr('src')) return;
+				
+				var checkForRetina = new RegExp("(.+)("+settings['retina_part']+"\\.\\w{3,4})");
+				if(checkForRetina.test($(element).attr('src'))) return;
 
 				var new_image_src = $(element).attr('src').replace(/(.+)(\.\w{3,4})$/, "$1"+ settings['retina_part'] +"$2");
 				$.ajax({url: new_image_src, type: "HEAD", success: function() {
